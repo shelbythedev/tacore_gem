5c0cc057a2627b9216bbd37551e875b51ed8639b
Brandon Criss <bcriss249@gmail.com>
2016-10-26 11:58:41 -0500

    Added the actual gateway files

--- lib/tacore.rb
+++ lib/tacore.rb
@@ -96,8 +96,7 @@ module TACore
   require 'tacore/client'
   require 'tacore/venue'
   require 'tacore/device'
-  require 'tacore/scan'
-  require 'tacore/movement'
+  require 'tacore/gateway'
 
 
   class Test < Auth
---
+++ lib/tacore/gateway.rb
@@ -0,0 +1,34 @@
+module TACore
+  # => Gateway class methods
+  class Gateway < Auth
+
+      # Shows the devices that are seen by the gateway id
+      # @param token [String] Client Token after Authentication
+      # @param client_id [String] used from {Client.create}
+      # @param gateway_id [String] Gateway ID
+      # @return [Array<Object, Object>] in JSON format
+      def self.sees(token, client_id, gateway_id)
+        request(:get, '/gateway/' + gateway_id.to_s + '/sees', {token: token, "client_id" => client_id})
+    end
+
+    # Gets the Gateway by ID
+    # @param token [String] Client Token after Authentication
+    # @param client_id [String] used from {Client.create}
+    # @param gateway_id [String] Gateway ID
+    # @return [Array<Object, Object>] in JSON format
+    def self.find(token, client_id, gateway_id)
+      request(:get, '/gateway/' + gateway_id.to_s, {token: token, "client_id" => client_id})
+    end
+
+    # Updates Gateway venue_id
+    # @param token [String] Client Token after Authentication
+    # @param client_id [String] used from {Client.create}
+    # @param gateway_id [String] Gateway ID
+    # @param gateway [Object]
+    # @return [Array<Object, Object>] in JSON format
+    def self.update(token, client_id, gateway_id, gateway = {})
+      request(:put, '/gateway/' + gateway_id.to_s, {token: token, "client_id" => client_id})
+    end
+
+  end
+end
